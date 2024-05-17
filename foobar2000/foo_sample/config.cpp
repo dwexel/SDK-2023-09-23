8e451946de23b2823849fc65ffdc4fd1635fa978
dex <dexterearley@gmail.com>
2024-05-17 15:49:47 -0500

    trim unnecessary files to make a release

---
+++ foobar2000/foo_sample/config.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+//
+//static constexpr GUID guid_advconfig_branch = { 0x27324fc4, 0x4400, 0x4a9a, { 0xa7, 0x7, 0x7c, 0xba, 0x22, 0xc9, 0x9c, 0x5e } };
+//static constexpr GUID guid_cfg_dir = { 0x1a4e74fd, 0xf5ef, 0x4fd0, { 0xaf, 0x24, 0x12, 0xdd, 0x11, 0x46, 0x88, 0x0 } };
+//static constexpr GUID guid_cfg_openafter = { 0xbaf9456c, 0xe2fe, 0x4f0c, { 0x89, 0x8, 0x34, 0x9c, 0xa4, 0xce, 0xba, 0x90 } };
+//
+//
+//static constexpr char default_cfg_dir[] = "C:\\Users\\dexte\\Sync\\testing2";
+//static constexpr bool default_cfg_openafter = true;
+//
+//static advconfig_branch_factory g_advconfigBranch("Dex component", guid_advconfig_branch, advconfig_branch::guid_branch_tools, 0);
+//static advconfig_string_factory cfg_dir("dir", guid_cfg_dir, guid_advconfig_branch, 0, default_cfg_dir);
+//static advconfig_checkbox_factory cfg_openafter("open after????????", guid_cfg_openafter, guid_advconfig_branch, 1, default_cfg_openafter);
+//
+//
--- foobar2000/foo_sample/mainmenu.cpp
+++ foobar2000/foo_sample/mainmenu.cpp
@@ -1,169 +1,223 @@
 #include "stdafx.h"
-
-#include "playback_stream_capture.h"
-
-
-// I am foo_sample and these are *my* GUIDs
-// Make your own when reusing code or else
-static const GUID guid_mainmenu_group_id = { 0x44963e7a, 0x4b2a, 0x4588, { 0xb0, 0x17, 0xa8, 0x69, 0x18, 0xcb, 0x8a, 0xa5 } };
-static const GUID guid_test = { 0x7c4726df, 0x3b2d, 0x4c7c,{ 0xad, 0xe8, 0x43, 0xd8, 0x46, 0xbe, 0xce, 0xa8 } };
-static const GUID guid_playbackstate = { 0xbd880c51, 0xf0cc, 0x473f,{ 0x9d, 0x14, 0xa6, 0x6e, 0x8c, 0xed, 0x25, 0xae } };
-static const GUID guid_io = { 0xd380c333, 0xa72c, 0x4e1e,{ 0x97, 0xca, 0xed, 0x14, 0xeb, 0x93, 0x76, 0x23 } };
-static const GUID guid_listcontrol_advanced = { 0x27e29db0, 0x3079, 0x4ce0, { 0x8b, 0x4a, 0xa0, 0x78, 0xeb, 0x6, 0x56, 0x86 } };
-static const GUID guid_listcontrol_simple = { 0x34664996, 0x54cd, 0x48eb, { 0xa8, 0x20, 0x8f, 0x45, 0x7d, 0xcc, 0xff, 0xbb } };
-static const GUID guid_listcontrol_ownerdata = { 0xc6d23696, 0x4be5, 0x4daa, { 0xaf, 0xb2, 0x35, 0x14, 0xa, 0x47, 0xd2, 0xf9 } };
-static const GUID guid_playback_stream_capture = { 0x3d0f0f1a, 0x6b5f, 0x42e3, { 0xa4, 0x6d, 0x49, 0x1, 0x3, 0xf0, 0x54, 0xb2 } };
-
-static const GUID guid_exp_l = { 0x15713569, 0x8d2b, 0x4916, { 0xa3, 0x3b, 0xc6, 0x1e, 0x84, 0x33, 0x8f, 0x53 } };
-static const GUID guid_exp_c = { 0x279db8a3, 0x47f9, 0x48b8, { 0x90, 0x16, 0x8a, 0x47, 0xf6, 0xd0, 0x5, 0xa7 } };
+#include "config.h"
 
 
 
-static mainmenu_group_popup_factory g_mainmenu_group(guid_mainmenu_group_id, mainmenu_groups::file, mainmenu_commands::sort_priority_dontcare, "Sample component");
 
-void RunPlaybackStateDemo(); //playback_state.cpp
-void RunIOTest(); // IO.cpp
-void RunListControlSimpleDemo(); // listcontrol-simple.cpp
-void RunListControlOwnerDataDemo(); // listcontrol-ownerdata.cpp
-void RunListControlAdvancedDemo(); // listcontrol-advanced.cpp
 
-void ExportPlaylistLinked() {
-	metadb_handle_list pl;
-	playlist_manager::get()->activeplaylist_get_all_items(pl);
+void link_some(const char* baseDir, pfc::string8 name, metadb_handle_list_cref p_list);
+void make_playlist(const char* baseDir, pfc::string8 name, metadb_handle_list_cref p_list);
+void reset(const char* baseDir);
 
-	// copied from the contextmenu.cpp
-	// get GUID by name?
-	// use the language to get it?
-	static const GUID guid_linkFiles = { 0x1d3c3fe9, 0x9ed3, 0x4539, { 0xbe, 0x31, 0x64, 0xb9, 0x68, 0xf0, 0x4, 0x40  } };
 
-	menu_helpers::run_command_context_ex(guid_linkFiles, pfc::guid_null, pl, guid_exp_l);
 
-	pfc::string8 name;
-	playlist_manager::get()->activeplaylist_get_name(name);
-	popup_message::g_show(fb2k::formatTrackList(pl), name);
+void link_some(const char* baseDir, pfc::string8 name, metadb_handle_list_cref p_list) {
+
+	// get filesystem pointer
+	filesystem::ptr fs = filesystem::get(baseDir);
+
+	name = pfc::io::path::replaceIllegalNameChars(name);
+
+	// get absolute path of the directory where files will be stored
+	pfc::string8 playlistDir = baseDir;
+	playlistDir.end_with(fs->pathSeparator());
+	playlistDir += name;
+
+	if (!fs->directory_exists(playlistDir, fb2k::noAbort)) {
+		fs->create_directory(playlistDir, fb2k::noAbort);
+	}
+
+	t_size n, m = p_list.get_count();
+	pfc::string8 inFN;
+	pfc::string8 outPath;
+
+	for (n = 0; n < m; n++) {
+
+		// get path of source file
+		const char* inPath = p_list.get_item(n)->get_path();
+
+		// get filename + extension of source file
+		fs->extract_filename_ext(inPath, inFN);
+
+		// make a destination path
+		outPath.copy(playlistDir);
+		outPath.end_with(fs->pathSeparator());
+		outPath += inFN;
+
+		if (fs->exists(outPath, fb2k::noAbort)) {
+			// FB2K_console_formatter() << outPath << " existed\n";
+		}
+		else {
+			try {
+				// prepend file:// to outPath
+				fs->g_link(inPath, outPath, fb2k::noAbort);
+			}
+			catch (pfc::exception e) {
+				pfc::string_formatter sf;
+				throw pfc::exception(sf << "error linking " << inPath << " to " << outPath << e);
+
+			}
+		}
+	}
+}
+
+// write m3u8 file
+void make_playlist(const char* baseDir, pfc::string8 name, metadb_handle_list_cref p_list) {
+	filesystem::ptr fs = filesystem::get(baseDir);
+
+	name = pfc::io::path::replaceIllegalNameChars(name);
+
+	t_size n, m = p_list.get_count();
+	pfc::string8 inFN;
+	pfc::string8 p_content;
+
+
+	for (n = 0; n < m; n++) {
+
+		const char* inPath = p_list.get_item(n)->get_path();
+
+		fs->extract_filename_ext(inPath, inFN);
+
+		p_content.add_string(name);
+		p_content.add_char(fs->pathSeparator());
+		p_content.add_string(inFN, inFN.get_length());
+		p_content.add_char('\n');
+	}
+
+	pfc::string8 playlistFn = name << ".m3u8";
+	pfc::string8 outPath = baseDir;
+	outPath.end_with(fs->pathSeparator());
+	outPath += playlistFn;
+
+	const double openTimeout = 1.0;
+	auto outLock = file_lock_manager::get()->acquire_write(outPath, fb2k::noAbort);
+
+	if (!p_content.is_valid_utf8()) {
+		// there was an error putting utf-8 text in the m3u8 file
+		// why might that be?
+		throw pfc::exception("here");
+	}
+
+	size_t p_size = p_content.get_length();
+	if (p_size > 0) {
+		fs->rewrite_file(outPath, fb2k::noAbort, openTimeout, p_content, p_size);
+	}
+}
+
+void reset(const char* baseDir) {
+	filesystem::ptr fs = filesystem::get(baseDir);
+	fs->g_remove(baseDir, fb2k::noAbort);
+	fs->g_create_directory(baseDir, fb2k::noAbort);
 }
 
 
-void ExportPlaylistCopy() {
-	metadb_handle_list pl;
-	playlist_manager::get()->activeplaylist_get_all_items(pl);
 
-	// ditto
-	static const GUID guid_copyFiles = { 0x7f8a6569, 0xe46b, 0x4698, { 0xaa, 0x30, 0xc4, 0xc1, 0x44, 0xc9, 0xc8, 0x92 } };
 
-	pfc::string8 name;
-	playlist_manager::get()->activeplaylist_get_name(name);
-	popup_message::g_show(fb2k::formatTrackList(pl), name);
 
-}
 
-class mainmenu_commands_sample : public mainmenu_commands {
-public:
-	enum {
-		cmd_test = 0,
-		cmd_playbackstate,
-		cmd_io,
-		cmd_listcontrol_simple,
-		cmd_listcontrol_ownerdata,
-		cmd_listcontrol_advanced,
-		cmd_playback_stream_capture,
-		cmd_exp_l,
-		cmd_exp_c,
-		cmd_total
+namespace { // main menu
+
+	static const GUID guid_mainmenu_group_id = { 0x86f3cea1, 0x57cc, 0x46ca, { 0x81, 0x7e, 0x61, 0x44, 0x8a, 0x24, 0x9e, 0xdc } };
+	static const GUID guid_test = { 0x2ab57cbc, 0x415b, 0x4521, { 0x80, 0xed, 0xd5, 0x8a, 0xe6, 0x1f, 0x1c, 0x2d } };
+
+	static mainmenu_group_factory g_mainmenu_group(guid_mainmenu_group_id, mainmenu_groups::file_playlist, mainmenu_commands::sort_priority_last);
+
+
+	class mainmenu_commands_sample : public mainmenu_commands {
+	public:
+		t_uint32 get_command_count() override {
+			return 1;
+		}
+		GUID get_command(t_uint32 p_index) override {
+			return guid_test;
+		}
+		void get_name(t_uint32 p_index, pfc::string_base& p_out) override {
+			p_out = "Push playlists ";
+		}
+		bool get_description(t_uint32 p_index, pfc::string_base& p_out) override {
+			p_out << "Push playlists (as hardlink) to " << cfg_dir.get();
+			return true;
+		}
+		GUID get_parent() override {
+			return guid_mainmenu_group_id;
+		}
+		void execute(t_uint32 p_index, service_ptr_t<service_base> p_callback) override {
+
+
+			playlist_manager::ptr playlist_manager = playlist_manager::get();
+
+			size_t n = playlist_manager->get_playlist_count();
+
+			
+
+
+			pfc::string8 baseDir; 
+			cfg_dir.get(baseDir);
+			
+			// idkkkkkkkkkkk
+			// this only works on fb2k 2.0 and up			
+			//auto baseDirRef = fb2k::configStore::get()->getConfigString("dir", default_cfg_dir);
+
+
+			
+				
+
+			pfc::string_formatter errorLog;
+
+			// get filesystem pointer
+			filesystem::ptr fs = filesystem::get(baseDir);
+
+			try {
+				reset(baseDir);
+			}
+			catch (pfc::exception e) {
+				popup_message::g_show(e.what(), "couldn't reset ... Blah");
+				//return;
+			}
+
+
+			// is this right
+			for (size_t i = 0; i < n; i++) {
+				pfc::string8 name;
+				playlist_manager->playlist_get_name(i, name);
+
+				metadb_handle_list items;
+				playlist_manager->playlist_get_all_items(i, items);
+
+				try {
+					make_playlist(baseDir, name, items);
+				}
+				catch (pfc::exception e) {
+					errorLog << "error writing playlist";
+				}
+
+				// not needed
+				// metadb_handle_list_helper::remove_duplicates(items);
+
+				try {
+					link_some(baseDir, name, items);
+				}
+				catch (pfc::exception e) {
+					errorLog << "error exporting playlist " << name << ": " << e << "\n\n";
+				}
+			}
+
+			if (errorLog.length() <= 0) {
+				errorLog << "no error from linking";
+			}
+			popup_message::g_show(errorLog, "Blah");
+
+
+#ifdef _WIN32
+			if (!cfg_openafter.get()) return;
+			if (!ModalDialogPrologue()) return;
+			const HWND wndParent = core_api::get_main_window();
+			uShellExecute(wndParent, "open", baseDir, NULL, NULL, SW_SHOWDEFAULT);
+#endif
+		}
+
 	};
-	t_uint32 get_command_count() override {
-		return cmd_total;
-	}
-	GUID get_command(t_uint32 p_index) override {
 
-		switch(p_index) {
-			case cmd_test: return guid_test;
-			case cmd_playbackstate: return guid_playbackstate;
-			case cmd_io: return guid_io;
-			case cmd_listcontrol_simple: return guid_listcontrol_simple;
-			case cmd_listcontrol_ownerdata: return guid_listcontrol_ownerdata;
-			case cmd_listcontrol_advanced: return guid_listcontrol_advanced;
-			case cmd_playback_stream_capture: return guid_playback_stream_capture;
-			case cmd_exp_l: return guid_exp_l;
-			case cmd_exp_c: return guid_exp_c;
-			default: uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
-		}
-	}
-	void get_name(t_uint32 p_index,pfc::string_base & p_out) override {
-		switch(p_index) {
-			case cmd_test: p_out = "Test command"; break;
-			case cmd_playbackstate: p_out = "Playback state demo"; break;
-			case cmd_io: p_out = "I/O test"; break;
-			case cmd_listcontrol_simple: p_out = "Simple CListControl demo"; break;
-			case cmd_listcontrol_ownerdata: p_out = "Owner-data CListControl demo"; break;
-			case cmd_listcontrol_advanced: p_out = "Advanced CListControl demo"; break;
-			case cmd_playback_stream_capture: p_out = "Playback stream capture demo"; break;
-			case cmd_exp_l: p_out = "Export playlist links"; break;
-			case cmd_exp_c: p_out = "Export playlist copies"; break;
-			default: uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
-		}
-	}
-	bool get_description(t_uint32 p_index,pfc::string_base & p_out) override {
-		switch(p_index) {
-			case cmd_test: p_out = "This is a sample menu command."; return true;
-			case cmd_playbackstate: p_out = "Opens the playback state demo dialog."; return true;
-			case cmd_io: p_out = "Runs I/O test."; return true;
-			case cmd_listcontrol_simple: p_out = "Runs Simple CListControl demo."; return true;
-			case cmd_listcontrol_ownerdata: p_out = "Runs Owner Data CListControl demo."; return true;
-			case cmd_listcontrol_advanced: p_out = "Runs Advanced CListControl demo."; return true;
-			case cmd_playback_stream_capture: p_out = "Toggles playback stream capture operation."; return true;
-			case cmd_exp_l: p_out = "Export playlist to directory+m3u8  using linking"; return true;
-			case cmd_exp_c: p_out = "Export playlist to directory+m3u8 using copying"; return true;
-			default: return false;
-		}
-	}
-	GUID get_parent() override {
-		return guid_mainmenu_group_id;
-	}
-	void execute(t_uint32 p_index,service_ptr_t<service_base> p_callback) override {
-		switch(p_index) {
-			case cmd_test:
-				popup_message::g_show("This is a sample menu command.", "Blah");
-				break;
-			case cmd_playbackstate:
-				RunPlaybackStateDemo();
-				break;
-			case cmd_io:
-				RunIOTest();
-				break;
-			case cmd_listcontrol_simple:
-				RunListControlSimpleDemo();
-				break;
-			case cmd_listcontrol_ownerdata:
-				RunListControlOwnerDataDemo();
-				break;
-			case cmd_listcontrol_advanced:
-				RunListControlAdvancedDemo();
-				break;
-			case cmd_playback_stream_capture:
-				ToggleCapture();
-				break;
-			case cmd_exp_l:
-				ExportPlaylistLinked();
-				break;
-			case cmd_exp_c:
-				ExportPlaylistCopy();
-				break;
-			default:
-				uBugCheck(); // should never happen unless somebody called us with invalid parameters - bail
-		}
-	}
-	bool get_display(t_uint32 p_index,pfc::string_base & p_text,t_uint32 & p_flags) override {
-		// OPTIONAL method
-		bool rv = mainmenu_commands::get_display(p_index, p_text, p_flags);
-		if (rv) switch(p_index) {
-		case cmd_playback_stream_capture:
-			// Add checkmark if capture is in progress
-			if ( IsCaptureRunning() ) p_flags |= flag_checked;
-			break;
-		}
-		return rv;
-	}
-};
+	static mainmenu_commands_factory_t<mainmenu_commands_sample> g_mainmenu_commands_sample_factory;
 
-static mainmenu_commands_factory_t<mainmenu_commands_sample> g_mainmenu_commands_sample_factory;
+}
